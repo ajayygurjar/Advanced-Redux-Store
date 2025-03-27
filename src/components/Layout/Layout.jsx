0c84a44cfb99a0e29c731865604e4c103d61f183
ajayygurjar <ajay1651@gmail.com>
2025-03-27 18:47:40 +0530

    integration of reduxtoolkit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+ import MainHeader from './MainHeader';
+ 
+ const Layout = () => {
+   return (
+     <Fragment>
+       <MainHeader />
+       <main><p>Hello Cart user</p></main>
+     </Fragment>
+   );
+ };
+ 
+ export default Layout;
